161bdf094877ea3a2643984c179a43dc9b127cd0
Dev Sonwani <sonwani.dev22@gmail.com>
2026-05-08 13:58:18 +0530

    initia commit for java project l

---
+++ demo/src/test/java/forgitignore/demo/CalculatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void testAdd() {
+
+        Calculator calculator = new Calculator();
+
+        assertEquals(5, calculator.add(2, 3));
+
+    }
+
+    @Test
+    void testSub() {
+
+        Calculator calculator = new Calculator();
+
+        assertEquals(-1, calculator.sub(4, 5));
+
+    }
+}
